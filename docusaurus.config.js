a0e9c1b0691e97be5d35a1e799607c3585a97a87
Cameron McKenzie <cwmckenz@gmail.com>
2024-02-28 10:36:34 -0500

    baseURL fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,49 +2,49 @@
 // `@type` JSDoc annotations allow editor autocompletion and type checking
 // (when paired with `@ts-check`).
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 /** @type {import('@docusaurus/types').Config} */
 
 const config = {
-  title: 'DiscOps Docs',
+  title: "DiscOps Docs",
   tagline: "Documentation for EY's DiscOps Project.",
-  favicon: 'img/logo.svg',
+  favicon: "img/logo.svg",
 
   // Production URL here:
-  url: 'https://stevenz1998.github.io/',
+  url: "https://stevenz1998.github.io/",
 
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/EPOCDoc/",
 
   // GitHub pages deployment config (Really, not needed).
-  organizationName: 'StevenZ1998',
-  projectName: 'stevenz1998.github.io',
-  deploymentBranch: 'main',
+  organizationName: "StevenZ1998",
+  projectName: "stevenz1998.github.io",
+  deploymentBranch: "main",
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Language localization.
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -53,61 +53,60 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-
       // Change here to modify navbar.
       navbar: {
-        title: 'Home',
+        title: "Home",
         logo: {
-          alt: 'Disc Ops Logo',
-          src: 'img/logo.svg',
+          alt: "Disc Ops Logo",
+          src: "img/logo.svg",
         },
 
         // Items in the navbar.
         items: [
           {
-            type: 'doc',
-            docId: 'docs/Getting Started/main',
-            sidebarID: 'docsSideBar',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "docs/Getting Started/main",
+            sidebarID: "docsSideBar",
+            position: "left",
+            label: "Docs",
           },
           {
-            type: 'doc',
-            docId: 'api/main',
-            sidebarID: 'apiSideBar',
-            position: 'left',
-            label: 'API',
+            type: "doc",
+            docId: "api/main",
+            sidebarID: "apiSideBar",
+            position: "left",
+            label: "API",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Home',
-                to: '/',
+                label: "Home",
+                to: "/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'EY',
-                to: 'https://www.ey.com/en_us',
+                label: "EY",
+                to: "https://www.ey.com/en_us",
               },
               {
-                href: 'https://github.com/StevenZ1998/DiscOps_Docs',
-                label: 'Docs GitHub',
-                position: 'right',
+                href: "https://github.com/StevenZ1998/DiscOps_Docs",
+                label: "Docs GitHub",
+                position: "right",
               },
               {
-                href: 'https://github.com/cmckenz87/discops2024',
-                label: 'DiscOps Github',
-                position: 'right',
+                href: "https://github.com/cmckenz87/discops2024",
+                label: "DiscOps Github",
+                position: "right",
               },
             ],
           },
